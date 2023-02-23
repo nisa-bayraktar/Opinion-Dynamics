fff6b942b6f207a96dd0f909f744475b6a164400
nisabayraktar <nisabayraktar.nb@gmail.com>
2023-02-23 16:19:00 +0000

    interactions applied and fixed

--- multi_reg.py
+++ multi_reg.py
@@ -21,6 +21,16 @@ print('matplotlib: {}'.format(matplotlib.__version__))
 df = pd.read_csv("async_1.csv")
 
 X = df[['c_values','h_values','a_values','th_values','ta_values']] 
+X['c:h'] = X['c_values'] * X['h_values']
+X['c:a'] = X['c_values'] * X['a_values']
+X['c:th'] = X['c_values'] * X['th_values']
+X['c:ta'] = X['c_values'] * X['ta_values']
+X['h:a'] = X['h_values'] * X['a_values']
+X['h:th'] = X['h_values'] * X['th_values']
+X['h:ta'] = X['h_values'] * X['ta_values']
+X['a:th'] = X['a_values'] * X['th_values']
+X['a:ta'] = X['a_values'] * X['ta_values']
+X['th:ta'] = X['th_values'] * X['ta_values']
 y_weight = df['avg_weight']
 y_std = df['std_of_avg_comm_state']
 y_range = df['range_of_comm_state']
@@ -39,6 +49,7 @@ pval_list = [est.pvalues for est in est_list]
 model_names = ['avg_weight', 'number_of_comm','modularity','range_of_comm_state','std_of_avg_comm_state']
 x_values =['c_values','h_values','a_values','th_values','ta_values']
 
+
 coef_table = pd.DataFrame(data=coef_list, columns=X.columns, index=model_names)
 coef_table = coef_table.T
 
@@ -56,13 +67,13 @@ for i, p in enumerate(pval_list):
         else:
             coef_table_c.iloc[j,i] = '{:.2f}'.format(coef_table_c.iloc[j,i])
 
-max_values = coef_table.max()
-min_values = coef_table.min()
+max_values = coef_table.iloc[:6].max()
+min_values = coef_table.iloc[:6].min()
 
 print(max_values,min_values)
 print(coef_table)
 
-fig, ax = plt.subplots(figsize=(10,5))
+fig, ax = plt.subplots(figsize=(15,10))
 ax.axis('off')
 ax.axis('tight')
 
@@ -77,6 +88,7 @@ for j in range(1, coef_table_c.shape[0] +1):
             table[(j, i)].get_text().set_color('red')
         elif coef_table.iloc[j - 1, i] == min_values[i]:
             table[(j, i)].get_text().set_color('blue')
+# create a new table with interaction values
 
 table.scale(1, 1)
 plt.show()
